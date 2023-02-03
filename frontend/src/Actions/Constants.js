360150759e8ee4bc3f1d0e192941e654a47b7226
pragatimishra9 <55681199+pragatimishra9@users.noreply.github.com>
2023-02-03 19:28:11 +0530

    Delete frontend directory

--- frontend/src/Actions/Constants.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-}
-
-export const BASE_URL = "http://127.0.0.1:8000"
